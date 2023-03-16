a1f59423f1dcab931920702b1f083e0b83139dea
Oly Erofeeva <o.leland@yandex.ru>
2023-03-16 23:07:51 +0100

    reworked page 404

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,13 +1,25 @@
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components'
-// import styles from './not-found-page.module.css'
+import { useNavigate } from 'react-router-dom'
+import { Routes } from '../routes'
+import styles from './not-found-page.module.css'
+import image404 from '../../images/404-page-image.png'
 
 const NotFoundPage = () => {
-  // TODO: make it beautiful, add a picture
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate(Routes.Main)
+  }
   return (
-    <div className="mt-30">
-      <h2 className="mb-10 text text_type_main-large">Page not found</h2>
-      <p className="mb-30 text text_type_main-medium text_color_inactive">The page you are trying to access does’t exist</p>
-      <Button htmlType="button">Go to main page</Button>
+    <div className={`mt-4 ${styles.pageContent}`}>
+      <span className="text text_type_digits-large">404</span>
+      <h2 className="mb-4 text text_type_main-large">Page not found</h2>
+      <img className={styles.image} src={image404} alt="Page not found" />
+      <p className="mt-4 mb-4 text text_type_main-medium text_color_inactive">
+        The page you are trying to access does’t exist
+      </p>
+      <Button htmlType="button" onClick={handleClick}>
+        Go to main page
+      </Button>
     </div>
   )
 }
