355387d06a549fa6c5dc335fba672380edcacc93
Oly Erofeeva <o.leland@yandex.ru>
2023-03-18 12:18:01 +0100

    refactor: order details component & modal

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import { DndProvider } from 'react-dnd'
-import { useNavigate } from 'react-router-dom'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import BurgerConstructor from '../../components/burger-constructor/burger-constructor'
 import BurgerIngredients from '../../components/burger-ingredients/burger-ingredients'
-import OrderDetails from '../../components/order-details/order-details'
+import OrderModal from '../../components/order-modal/order-modal'
 import { ingredientsFetchProgressSelector } from '../../services/selectors/selectors'
-import { Ingredient, Progress } from '../../utils/types'
 import { placeOrderMiddleware } from '../../services/thunks/place-order-middleware'
 import { CookieName, getCookie } from '../../utils/cookie'
-import styles from './main-page.module.css'
+import { Ingredient, Progress } from '../../utils/types'
 import { Routes } from '../routes'
+import styles from './main-page.module.css'
 
 const MainPage = () => {
   const [isOrderDetailsModalOpen, setOrderDetailsModalOpen] = useState(false)
@@ -46,7 +46,7 @@ const MainPage = () => {
           </DndProvider>
         </main>
       )}
-      {isOrderDetailsModalOpen && <OrderDetails onClose={handleOrderDetailsClose} />}
+      {isOrderDetailsModalOpen && <OrderModal onClose={handleOrderDetailsClose} />}
     </>
   )
 }
